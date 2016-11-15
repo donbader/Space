56d80b892a6449cbba997bb02da856be3bc9118c
LaM <laochanlam@gmail.com>
2016-11-15 20:46:14 +0800

    homework add

---
+++ http/mongodb.js
@@ -0,0 +1,54 @@
+#!/usr/local/bin/node
+
+var querystring = require('querystring');
+var para = querystring.parse(process.env.QUERY_STRING);
+var database = require('./name.json');
+
+
+var express = require('express');
+var mongoose = require('mongoose');
+var app = express();
+mongoose.connect('mongodb://wp2016_groupL:66666666@localhost/wp2016_groupL');
+var db = mongoose.connection;
+
+app.get('/',function(req,res){
+  res.send("hello");
+});
+
+db.on('error',console.error.bind(console,'connection error:'));
+db.once('open', function callback(){
+  console.log('fk');
+  });
+
+app.listen(9487,function(){
+  console.log('listening 9487');
+  });
+
+var kittySchema = mongoose.Schema({
+    name: String
+  });
+
+
+kittySchema.methods.speak = function(){
+  var greeting = this.name ? "I am " + this.name : "Who am I" ;
+  console.log(greeting);
+}
+
+var Kitten = mongoose.model('Kittten', kittySchema);
+
+
+var lam = new Kitten({ name: 'yo'});
+console.log(lam.name);
+
+lam.speak();
+
+lam.save(function(err,lam){
+  if (err) return console.error(err);
+  lam.speak();
+});
+
+Kitten.find(function(err,kittens){
+  if (err) return console.error(err);
+  console.log(kittens);
+});
+
