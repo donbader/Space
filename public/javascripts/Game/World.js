d7cf04677de834f5b77677fdb4aa5383fe58952d
0xYTF <qko589999@gmail.com>
2016-11-29 19:41:38 +0800

    room testing

--- public/javascripts/Game/World.js
+++ public/javascripts/Game/World.js
@@ -1,17 +1,55 @@
 (function(){
-var World = this.World = Class.extend(new THREE.Object3D());
+var World = this.World = Class.extend({
+	init: function(long, width, height){
+		this.__proto__ = new THREE.Object3D();
 
-World.init = function(){
-	console.log("HIHI, this is world created");
-}
+		long = long || 1024;
+		width = width || 1024;
+		height = height || 300;
 
-var world = new World();
-var box = new THREE.Mesh(new THREE.BoxGeometry( 200, 20, 20 ), new THREE.MeshPhongMaterial( { color: 0xffff00 } ));
-box.position.set(0, 0, 6);
-box.name = 'box';
-world.add(box);
+		// ground
+		this.ground = new THREE.Mesh(
+						new THREE.PlaneGeometry(width, long)
+						, new THREE.MeshPhongMaterial( { color: 0xff00ff } ));
+		this.ground.rotation.x = -Math.PI / 2;
+		this.add(this.ground);
 
-console.log(world);
+
+
+		// walls
+		this.walls = [];
+		var wallsGeo = [
+            new THREE.PlaneGeometry(long, height),
+            new THREE.PlaneGeometry(width, height),
+            new THREE.PlaneGeometry(long, height),
+            new THREE.PlaneGeometry(width, height)
+		];
+		var wallsMaterial = new THREE.MeshLambertMaterial({ color: 0xff00ff });
+		for(var i in wallsGeo){
+			var mesh = new THREE.Mesh(wallsGeo[i], wallsMaterial);
+			mesh.position.y = height / 2;
+			this.walls.push(mesh);
+			this.add(this.walls[i]);
+		}
+        this.walls[0].rotation.y = -Math.PI / 2;
+        this.walls[0].position.x = width / 2;
+        this.walls[1].rotation.y = Math.PI;
+        this.walls[1].position.z = long / 2;
+        this.walls[2].rotation.y = Math.PI / 2;
+        this.walls[2].position.x = -width / 2;
+        this.walls[3].position.z = -long / 2;
+
+
+        // light
+        this.light = new THREE.PointLight();
+        this.light.position.set(0,height,0);
+        this.add(this.light);
+	}
+
+
+});
+
+
 })();
 
 
