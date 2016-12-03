14a2c2594f47c7dbf65b6d24843f6c3da24f0519
LaM <laochanlam@gmail.com>
2016-12-04 02:44:43 +0800

    login req & res done

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,28 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 var login = require('../db/login');	
+var querystring = require("querystring");
 
 /* GET home page. */
 router.get('/', function(req, res, next){
 	console.log("[GET] '/'")
-	login.doFinal(req.url);
-	
 	res.sendFile(path.join(__dirname, '../', 'index.html'));
   console.log(path.join(__dirname, '../', 'index.html'));
 });
+
+router.post('/login', function(req, res){
+	var body = '';
+	req.on('data', function(data){
+		body+=data;
+	});
+	req.on('end',function(){
+		querystring.parse(body);
+		//console.log(body);
+	})
+	res.send(body);
+});
+
+
 
 router.get('/About', function(req, res){
 	res.sendFile(path.join(__dirname, '../', 'About.html'));
