7b7466ac33e4230e340249869db95290089e1e3d
LaM <laochanlam@gmail.com>
2016-12-07 03:28:31 +0800

    upload website complete

--- routes/upload.js
+++ routes/upload.js
@@ -3,18 +3,41 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 var mongoose = require('mongoose');
+var querystring = require("querystring");
 
 router.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../view', 'Upload.html'));
 });
 
-router.get('/do', function(req, res) {
-    console.log("here");
-    // element.save(function(err, element) {
-    //     if (err) return console.error(err);
+router.post('/do', function(req, res) {
 
-    res.send("hi");
-    //  });
+  var body = '';
+  req.on('data', function(data) {
+      body += data;
+  });
+  req.on('end', function() {
+
+      var para = JSON.parse(body);
+
+      var id = para.id;
+      var type = para.type;
+      var data = para.data;
+
+      var element = new Item({
+          "id": id,
+          "type": type,
+          "data": data
+      });
+
+
+
+    element.save(function(err, element) {
+        if (err) return console.error(err);
+      });
+
+    res.send("success");
+
+  })
 });
 
 
